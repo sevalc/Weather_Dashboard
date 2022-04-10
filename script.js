6059629cecf3330c866ef14dbec4f2511d6bfab5
Seval Cakir <sevalc@live.com>
2022-04-09 23:47:58 -0700

    forecast fetch and  error state added to js

--- script.js
+++ script.js
@@ -1,45 +1,102 @@
 //create variable for fetching and storing data
 
 var apiKey = "019cb097ec31d6ff5a6b7d56b3297441";
-var city = "seattle"
-var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial"
+var cityInputError = false;
 
-//fetch info when a city name entered as input 
+//fetch info when a city name entered as input
+function cityFetch(city) {
+  var requestURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey +
+    "&units=imperial";
 
-fetch(requestURL).then(function(response) {
-    return response.json();
-    
-}).then(function(readableData) {
-    console.log(readableData)
-    var cityName = document.getElementById("city-name");
-    cityName.textContent = readableData.name;
-    var date = moment().format("M, D, YYYY");
-    document.getElementById("currentDay").textContent = date;
-    var cityHumidity = readableData.main.humidity;
-    document.getElementById("humidity").textContent = "Humidity: " + cityHumidity + "%";
-    var cityTemperature = readableData.main.temp;
-    document.getElementById("temperature").textContent = "Temperature: " + cityTemperature + " F";
-    var citywindSpeed = readableData.wind.speed;
-    document.getElementById("wind").textContent = "Wind Speed: " + citywindSpeed + "MPH";
-   
-    }); 
- 
+  fetch(requestURL)
+    .then(function (response) {
+      if (response.status !== 200) {
+        alert("wrong city name");
+        cityInputError = true;
+      } else {
+        cityInputError = false;
+      }
+      return response.json();
+    })
+    .then(function (readableData) {
+      if (cityInputError) {
+        return;
+      }
+      var cityName = document.getElementById("city-name");
+      cityName.textContent = readableData.name;
+      var date = moment().format("M, D, YYYY");
+      document.getElementById("currentDay").textContent = date;
+      var cityHumidity = readableData.main.humidity;
+      document.getElementById("humidity").textContent =
+        "Humidity: " + cityHumidity + " %";
+      var cityTemperature = readableData.main.temp;
+      document.getElementById("temperature").textContent =
+        "Temperature: " + cityTemperature + " F";
+      var citywindSpeed = readableData.wind.speed;
+      document.getElementById("wind").textContent =
+        "Wind Speed: " + citywindSpeed + " MPH";
+    });
 
-    //provide city name, date, icon for weather condition as header on card 
-    //provide temp, humidity, wind speed, UV index 
-        //UV index shows a color represents favorable, moderate or severe
-//add input to html to dispay
-//show 5 day future conditions for the same city 
-var requestURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial"
+  //provide city name, date, icon for weather condition as header on card
+  //provide temp, humidity, wind speed, UV index
+  //UV index shows a color represents favorable, moderate or severe
+  //add input to html to dispay
+  //show 5 day future conditions for the same city
+  var requestURLForecast =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=" +
+    apiKey +
+    "&units=imperial";
+  fetch(requestURLForecast)
+    .then(function (response) {
+      if (response.status !== 200) {
+        cityInputError = true;
+      } else {
+        cityInputError = false;
+      }
+      return response.json();
+    })
+    .then(function (readableDataForecast) {
+      if (cityInputError) {
+        return;
+      }
+      //console.log(readableDataForecast);
+      for (var writeIndex = 0; writeIndex < 5; writeIndex++) {
+        var readIndex = writeIndex * 8;
 
-fetch(requestURLForecast).then(function(response) {
-    return response.json();
-    
-}).then(function(readableDataForecast) {
-    console.log(readableDataForecast)
-    
-})
+        var forecastAtTimePoint = readableDataForecast.list[readIndex];
 
-//when a new city is searched, add last city to search history 
-//when clicked on a city at search history, present same data as before 
+        document.getElementById("currentDayCard" + writeIndex).textContent =
+          forecastAtTimePoint.dt_txt;
 
+        var cityHumidity1 = forecastAtTimePoint.main.humidity;
+        document.getElementById("humidityCard" + writeIndex).textContent =
+          "Humidity: " + cityHumidity1 + " %";
+
+        var cityTemperature1 = forecastAtTimePoint.main.temp;
+        document.getElementById("temperatureCard" + writeIndex).textContent =
+          "Temperature: " + cityTemperature1 + " F";
+
+        var citywindSpeed1 = forecastAtTimePoint.wind.speed;
+        document.getElementById("windCard" + writeIndex).textContent =
+          "Wind Speed: " + citywindSpeed1 + " MPH";
+      }
+    });
+}
+
+//when a new city is searched, add last city to search history
+//when clicked on a city at search history, present same data as before
+cityFetch("Seattle");
+
+document.getElementById("searchBtn").addEventListener("click", function () {
+  var cityInput = document.getElementById("textarea1").value;
+  if (cityInput === "") {
+    return;
+  }
+  cityFetch(cityInput);
+});
